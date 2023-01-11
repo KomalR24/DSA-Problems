c4a3ca91f1f7b38f24cff96193b8452b9ff249b9
Komal Ratre <78158668+KomalR24@users.noreply.github.com>
2023-01-11 16:22:47 +0530

    Add files via upload

---
+++ Binary Trees/Tree Isomorphism Problem/Solution.java
@@ -0,0 +1,16 @@
+//Link To Question : https://practice.geeksforgeeks.org/problems/check-if-tree-is-isomorphic/1
+
+class Solution
+{
+    boolean isIsomorphic(Node root1, Node root2)
+    {
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+
+        if(root1.data != root2.data) return false;
+
+        return isIsomorphic(root1.left,root2.left) && isIsomorphic(root1.right,root2.right) ||
+                isIsomorphic(root1.left, root2.right) && isIsomorphic(root1.right,root2.left);
+    }
+
+}
