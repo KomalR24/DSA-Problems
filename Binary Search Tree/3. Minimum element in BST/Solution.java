ed32bdc2822612c113ac60be4c25dbb15b26b5f3
Komal Ratre <78158668+KomalR24@users.noreply.github.com>
2023-01-11 19:05:39 +0530

    Update Solution.java

--- Binary Search Tree/3. Minimum element in BST/Solution.java
+++ Binary Search Tree/3. Minimum element in BST/Solution.java
@@ -1,6 +1,6 @@
 //Link To Question : https://practice.geeksforgeeks.org/problems/minimum-element-in-bst/1
 
-class Tree {
+class Solution {
     int minValue(Node root) {
         if(root==null) return -1;
         if(root.left==null) return root.data;
