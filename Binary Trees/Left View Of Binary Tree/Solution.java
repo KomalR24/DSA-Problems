05a433da74bd2bde294d43f89ce2c20d04df0083
Komal Ratre <78158668+KomalR24@users.noreply.github.com>
2023-01-11 14:26:39 +0530

    Add files via upload

---
+++ Binary Trees/Left View Of Binary Tree/Solution.java
@@ -0,0 +1,28 @@
+//Link To Question: https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1
+
+class Solution
+{
+    ArrayList<Integer> leftView(Node root)
+    {
+        ArrayList<Integer> al = new ArrayList<>();
+        Queue<Node> q = new LinkedList<Node>();
+        if(root == null) return al;
+        q.add(root);
+        while(q.isEmpty()==false){
+            int count = q.size();
+            for(int i=0;i<count;i++){
+                Node curr = q.poll();
+                if(i==0){
+                    al.add(curr.data);
+                }
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
+        }
+        return al;
+    }
+}
