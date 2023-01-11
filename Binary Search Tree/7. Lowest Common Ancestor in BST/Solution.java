95cd96db0a7a605eef40f7b6e5afb5b5d1831720
Komal Ratre <78158668+KomalR24@users.noreply.github.com>
2023-01-11 19:20:59 +0530

    Add files via upload

---
+++ Binary Search Tree/7. Lowest Common Ancestor in BST/Solution.java
@@ -0,0 +1,12 @@
+//Link To Question : https://practice.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-bst/1
+
+class Solution
+{
+    Node LCA(Node root, int n1, int n2)
+    {
+        if(n1<root.data && n2<root.data) return LCA(root.left,n1,n2);
+        else if(n1>root.data && n2>root.data) return LCA(root.right,n1,n2);
+        else return root;
+    }
+
+}
