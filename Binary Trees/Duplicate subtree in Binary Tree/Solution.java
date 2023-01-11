372900659cb2d9d7bff9953f093b7356ff08196e
Komal Ratre <78158668+KomalR24@users.noreply.github.com>
2023-01-11 15:34:09 +0530

    Add files via upload

---
+++ Binary Trees/Duplicate subtree in Binary Tree/Solution.java
@@ -0,0 +1,32 @@
+//Link To Question: https://practice.geeksforgeeks.org/problems/duplicate-subtree-in-binary-tree/1
+
+class Solution {
+    HashMap<String , Integer> map = new HashMap<>();
+    int dupSub(Node root) {
+        add(root);
+        for(int item: map.values()) {
+            if (item >= 2) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+    public  String add(Node root){
+        if(root==null){
+            return "$";
+        }
+        if(root.left==null && root.right==null){
+            String s = String.valueOf(root.data);
+            return s;
+        }
+        String left = add(root.left);
+        String right  = add(root.right);
+        String s =  (left + " " +String.valueOf(root.data) + " "+right);
+        if(map.containsKey(s)){
+            map.put(s , map.get(s)+1);
+        }else{
+            map.put(s,1);
+        }
+        return s;
+    }
+}
