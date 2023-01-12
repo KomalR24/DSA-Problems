6ef4c724cdc8859ab5fd13fb32b92ebe97aaea09
Komal Ratre <78158668+KomalR24@users.noreply.github.com>
2023-01-12 12:39:31 +0530

    Add files via upload

---
+++ Binary Search Tree/Normal BST to Balanced BST/Solution.java
@@ -0,0 +1,34 @@
+// Link To Question: https://practice.geeksforgeeks.org/problems/normal-bst-to-balanced-bst/1
+
+class GfG
+{
+    Node buildBalancedTree(Node root)
+    {
+        if(root== null){
+            return null;
+        }
+        ArrayList<Integer> al = new ArrayList<>();
+        inorder(root,al);
+        Node ans =inordertobst(al,0,al.size()-1);
+        return ans;
+    }
+    Node inordertobst(ArrayList<Integer> al , int start, int end ){
+        if(start>end){
+            return null;
+        }
+        int mid = (start + end)/2;
+        Node newnode = new Node(al.get(mid));
+        newnode.left=inordertobst(al,start,mid-1);
+        newnode.right=inordertobst(al,mid+1, end);
+        return newnode;
+    }
+    void inorder(Node root, ArrayList<Integer> al){
+        if(root==null){
+            return ;
+        }
+        inorder(root.left, al);
+        al.add(root.data);
+        inorder(root.right,al);
+        return;
+    }
+}
