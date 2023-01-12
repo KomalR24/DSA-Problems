ad531fbb71e159259dd3338f441b723c6e19ccf9
Komal Ratre <78158668+KomalR24@users.noreply.github.com>
2023-01-12 12:32:20 +0530

    Add files via upload

---
+++ Binary Search Tree/Check for BST/Solution.java
@@ -0,0 +1,26 @@
+// Link To Question: https://practice.geeksforgeeks.org/problems/check-for-bst/1
+
+class Solution
+{
+    boolean isBST(Node root)
+    {
+        if(root==null){
+            return false;
+        }
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        inOrder(root,al);
+        for(int i=0;i<al.size()-1;i++){
+            if(al.get(i)>al.get(i+1))
+                return false;
+        }
+        return true;
+    }
+    public void inOrder(Node root,ArrayList<Integer>al){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left,al);
+        al.add(root.data);
+        inOrder(root.right,al);
+    }
+}
