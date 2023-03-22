5ec5ef1deece7fb3ba5a9eecff822965e2e68621
Komal Ratre <78158668+KomalR24@users.noreply.github.com>
2023-03-22 15:57:20 +0530

    Update Solution.java

--- Arrays/Reverse the array/Solution.java
+++ Arrays/Reverse the array/Solution.java
@@ -1,4 +1,4 @@
-package Solution;
+// Link To Article:  https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
 
 class Solution {
     static void revereseArray(int arr[], int start, int end)
