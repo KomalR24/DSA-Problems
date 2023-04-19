f81f4f31eaf999e4438b881d2cc155a7be8ceba2
Komal Ratre <78158668+KomalR24@users.noreply.github.com>
2023-04-19 19:29:12 +0530

    Add files via upload

---
+++ Matrix/Spiral Traversal/Solution.java
@@ -0,0 +1,38 @@
+// Link to Question: https://leetcode.com/problems/spiral-matrix/description/
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> al = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int top = 0;
+        int bottom = m-1;
+        int left = 0;
+        int right = n-1;
+
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                al.add(matrix[top][i]);
+            }
+            top++;
+
+            for(int i=top;i<=bottom;i++){
+                al.add(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    al.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    al.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return al;
+    }
+}
